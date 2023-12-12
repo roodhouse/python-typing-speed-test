6f4ea24fc6e9ceb8a0c1a4c0fe3497fcd53b6899
roodhouse <rughjm@gmail.com>
2023-12-12 16:59:30 -0600

    frames done

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-# create interface
-# title
-# directions
-# input space
 # make it interactive
+    # connect the bottom frame to the words in the middle Frame
+    # highlight current word
+    # indicate correct and incorrect characters pushed
+    # upon space move to next word in list
+    # if close to the end then bring in and append a new list of words
 # calc score
 
 from tkinter import *  # noqa: F403
@@ -10,7 +11,7 @@ from random_word import RandomWords, Wordnik
 
 window = Tk()  # noqa: F405
 window.title("Typing Speed Test")
-window.geometry("600x400") # adjust the size here
+window.geometry("600x525")  # adjust the size here
 
 canvas = Canvas(width=600, height=200, background="#800808")  # noqa: F405\
 canvas.pack()
@@ -42,19 +43,6 @@ directions = canvas.create_text(
     fill="white",
 )
 
-# 4 frames
-# mainframe
-    # top frame
-        # corrected CPM
-        # WPM
-        # timer
-        # restart
-            # bind to restart program
-    # middle frame
-        # random words
-    # bottom frame
-        # where user types
-
 def top_frame(container):
     frame = Frame(container)  # noqa: F405
 
@@ -65,43 +53,78 @@ def top_frame(container):
     frame.columnconfigure(5, weight=1)
     frame.columnconfigure(6, weight=3)
     frame.columnconfigure(7, weight=1)
-    
+
     # CPM
-    Label(frame, text='Corrected CPM: ').grid(column=0, row=0)  # noqa: F405
+    Label(frame, text="Corrected CPM: ").grid(column=0, row=0)  # noqa: F405
     corrected_cpm = Entry(frame, width=10)  # noqa: F405
     corrected_cpm.grid(column=1, row=0)
 
     # WPM
-    Label(frame, text='WPM: ').grid(column=3, row=0)  # noqa: F405
+    Label(frame, text="WPM: ").grid(column=3, row=0)  # noqa: F405
     words_per_min = Entry(frame, width=10)  # noqa: F405
     words_per_min.grid(column=4, row=0)
 
     # Time left
-    Label(frame, text='Time Left: ').grid(column=5, row=0)  # noqa: F405
-    time_left = Entry(frame, width=10) # need timer here  # noqa: F405
+    Label(frame, text="Time Left: ").grid(column=5, row=0)  # noqa: F405
+    time_left = Entry(frame, width=10)  # need timer here  # noqa: F405
     time_left.grid(column=6, row=0)
 
     # Restart
-    Label(frame, text='Restart', foreground='white', font=('Arial', 12, 'underline'), cursor='man' ).grid(column=7, row=0)  # noqa: F405
+    Label(
+        frame,
+        text="Restart",
+        foreground="white",
+        font=("Arial", 12, "underline"),
+        cursor="man",
+    ).grid(column=7, row=0)  # noqa: F405
 
     return frame
+
 
 wordnik_service = Wordnik()
 
 random_words = wordnik_service.get_random_words()
 
+
 def middle_frame(container):
     frame = Frame(container)  # noqa: F405
     frame.columnconfigure(0, weight=1)
 
-    words_text = ' '.join(random_words)
+    words_text = " ".join(random_words)
 
-    text_box = Text(frame, width=60, height=10, wrap=WORD, font=('Arial', 15), padx=20, pady=20)  # noqa: F405
+    text_box = Text(
+        frame, width=60, height=10, wrap=WORD, font=("Arial", 15), padx=20, pady=20
+    )  # noqa: F405
     text_box.insert(END, words_text)  # noqa: F405
     text_box.config(state=DISABLED)  # noqa: F405
     text_box.pack(fill=X)  # noqa: F405
 
     return frame
+
+def bottom_frame(container):
+    frame = Frame(container)
+    frame.columnconfigure(0, weight=1)
+
+    text_box = Text(
+        frame, width=60, height=1, wrap=WORD, font=("Arial", 15), padx=20, pady=20,
+    )  # noqa: F405
+
+    def on_entry(event):
+        if text_box.get("1.0", "end-1c") == "type the words here...":
+            text_box.delete("1.0", "end")
+
+    def on_exit(event):
+        if text_box.get("1.0", "end-1c") == "":
+            text_box.insert("1.0", "type the words here...")
+
+    text_box.insert("1.0", "type the words here...")
+    text_box.pack(fill=X)  # noqa: F405
+
+    text_box.bind("<FocusIn>", on_entry)
+    text_box.bind("<FocusOut>", on_exit)
+
+    return frame
+
 
 def main_frame(parent):
     main_frame = Frame(parent)  # noqa: F405
@@ -114,9 +137,12 @@ def main_frame(parent):
     main_frame.rowconfigure(2, weight=2)
 
     top = top_frame(main_frame)
-    top.grid(row=0, column=0, sticky='ew')
+    top.grid(row=0, column=0, sticky="ew")
     middle = middle_frame(main_frame)
     middle.grid(row=1, column=0)
+    bottom = bottom_frame(main_frame)
+    bottom.grid(row=2, column=0)
+
 
 container_frame = Frame(window)  # noqa: F405
 container_frame.pack()
